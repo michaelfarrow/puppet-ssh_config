7031fe47536ccddca1a90e9c6a92e7c07db7848c
Mike Farrow <contact@mikefarrow.co.uk>
2015-04-01 17:32:09 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Puppet SSH Config Module
+
+```ruby
+mod "puppetlabs/stdlib", "4.5.1"
+
+# dep: puppetlabs/stdlib
+mod "puppetlabs/concat", "1.2.0"
+
+# dep: puppetlabs/stdlib
+# dep: puppetlabs/concat
+mod "saz/ssh", "2.5.0"
+```
---
+++ manifests/init.pp
@@ -0,0 +1,24 @@
+class ssh_config {
+
+	$ssh_keys = hiera_hash('accounts::ssh_keyse', {});
+
+	if $ssh_keys.keys.length == 0 {
+		$password_auth = 'yes'
+	} else {
+		$password_auth = 'no'
+	}
+
+	class { 'ssh':
+		server_options => {
+			'PasswordAuthentication' => $password_auth,
+			'Match Group ftpusers' => {
+				'ChrootDirectory' => '%h',
+				'ForceCommand' => 'internal-sftp',
+				'PasswordAuthentication' => 'yes',
+				'AllowTcpForwarding' => 'no',
+				'X11Forwarding' => 'no',
+			},
+		},
+	}
+
+}
